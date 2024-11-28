e6bef3b4218211559f312acea17fdc46c37a8911
rbw-tech <radipta111@gmail.com>
2024-11-28 23:43:28 +0700

    Update script-dom.js

--- script-dom.js
+++ script-dom.js
@@ -1,24 +1,29 @@
+// Nyari semua div yang ada, terus dikasih warna coklat
 const containers = document.querySelectorAll('div');
 containers.forEach(container => {
-    container.style.backgroundColor = '#6c4b15'; // Brownish color
+   container.style.backgroundColor = '#6c4b15'; // Warna coklat
 });
 
+// Ngubah link yang tadinya # jadi ke Google
 const link = document.querySelector('a');
 if (link) {
-    link.href = 'https://www.google.com';
+   link.href = 'https://www.google.com';
 }
 
+// Nyari semua list, terus diubah textnya pake List Baru 1,2,3
+// Sekalian stylenya diubah jadi warna putih, background abu-abu, font Arial ukuran 18px  
 const listItems = document.querySelectorAll('.list');
 listItems.forEach((item, index) => {
-    item.textContent = `List Baru ${index + 1}`;
-    
-    item.style.color = 'white';
-    item.style.backgroundColor = '#808080';
-    item.style.fontSize = '18px';
-    item.style.fontFamily = 'Arial';
+   item.textContent = `List Baru ${index + 1}`;
+   
+   item.style.color = 'white';
+   item.style.backgroundColor = '#808080';
+   item.style.fontSize = '18px';
+   item.style.fontFamily = 'Arial';
 });
 
+// Nyari semua paragraf terus diubah jadi warna putih
 const paragraphs = document.querySelectorAll('p');
 paragraphs.forEach(p => {
-    p.style.color = 'white';
+   p.style.color = 'white';
 });
