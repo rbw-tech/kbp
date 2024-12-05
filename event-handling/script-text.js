84785d8266d6f0a3faf35635dbd3d25d96866955
rbw-tech <radipta111@gmail.com>
2024-12-05 23:40:11 +0700

    Update and rename script.js to script-text.js

---
+++ event-handling/script-text.js
@@ -0,0 +1,6 @@
+const colorButton = document.getElementById('colorButton');
+const textParagraph = document.getElementById('textParagraph');
+
+colorButton.addEventListener('click', function() {
+    textParagraph.style.color = 'blue';
+});
